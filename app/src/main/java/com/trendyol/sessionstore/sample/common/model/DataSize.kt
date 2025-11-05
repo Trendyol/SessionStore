8da28b4e0f779d174f5cd2d97246c06c6755a899
ertugrul <ertugrulkaragoz12@gmail.com>
2025-11-05 13:05:40 +0300

    Reduce data sizes for sample app

--- app/src/main/java/com/trendyol/sessionstore/sample/common/model/DataSize.kt
+++ app/src/main/java/com/trendyol/sessionstore/sample/common/model/DataSize.kt
@@ -1,9 +1,9 @@
 package com.trendyol.sessionstore.sample.common.model
 
 enum class DataSize(val bytes: Int, val displayName: String, val exceedsBundleLimit: Boolean) {
-    SMALL(512 * 1024, "Small (512KB)", false),
-    MEDIUM(1024 * 1024, "Medium (1MB)", false),
-    LARGE((1.5 * 1024 * 1024).toInt(), "Large (1.5MB)", true),
-    XLARGE(3 * 1024 * 1024, "X-Large (3MB)", true),
-    XXLARGE(5 * 1024 * 1024, "XX-Large (5MB)", true)
+    SMALL(128 * 1024, "Small (128KB)", false),
+    MEDIUM(256 * 1024, "Medium (256KB)", false),
+    LARGE(512 * 1024, "Large (512KB)", false),
+    XLARGE(1024 * 1024, "X-Large (1MB)", false),
+    XXLARGE(2 * 1024 * 1024, "XX-Large (2MB)", true)
 }
